bd39a4d37a842f405d107a1ae34b60ac56e8f365
DHANUSH REDDY C S <reddydhanush779@gmail.com>
2026-03-03 13:55:01 +0530

    Add write_file function and update tests with new file handling

--- tests.py
+++ tests.py
@@ -1,24 +1,30 @@
 
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 def main():
     working_dir = "calculator"
 
-    root_contents = get_files_info(working_dir)
-    print(root_contents)
-    pkg_contents = get_files_info(working_dir, "pkg")
-    print(pkg_contents)
-    pkg_contents = get_files_info(working_dir, "/bin")
-    print(pkg_contents)
-    pkg_contents = get_files_info(working_dir, "../")
-    print(pkg_contents)
+    # root_contents = get_files_info(working_dir)
+    # print(root_contents)
+    # pkg_contents = get_files_info(working_dir, "pkg")
+    # print(pkg_contents)
+    # pkg_contents = get_files_info(working_dir, "/bin")
+    # print(pkg_contents)
+    # pkg_contents = get_files_info(working_dir, "../")
+    # print(pkg_contents)
 
-    print(get_file_content(working_dir, "lorem.txt"))
-    print(get_file_content(working_dir, "main.py"))
-    print(get_file_content(working_dir, "pkg/calculator.py"))   
-    print(get_file_content(working_dir, "/bin/cat"))   
-    print(get_file_content(working_dir, "pkg/does_not_exist.py"))   
+    # print(get_file_content(working_dir, "lorem.txt"))
+    # print(get_file_content(working_dir, "main.py"))
+    # print(get_file_content(working_dir, "pkg/calculator.py"))   
+    # print(get_file_content(working_dir, "/bin/cat"))   
+    # print(get_file_content(working_dir, "pkg/does_not_exist.py"))   
+
+    print(write_file(working_dir, "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file(working_dir, "pkg/morelorem.txt", "lorem ipsum dolor sit amet")) 
+    print(write_file(working_dir, "/tmp/temp.txt", "this should not be allowed")) 
+    print(write_file(working_dir, "pkg2/temp.txt", "this should be allowed")) 
 
 main()
